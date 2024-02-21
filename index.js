b13e20ec6019241350796faee8e81ec5d225a015
Ftulioalmeida <f.tulioalmeida@gmail.com>
2024-02-21 02:33:35 -0300

    Atualizando tabelas js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Connection } = require('pg');
-const { Sequelize } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 
 const sequelize = new Sequelize('API', 'postgres', 'postgres', {
@@ -17,3 +17,70 @@ Conectar = async () => {
 }
 
 Conectar()
+
+const Usuario = sequelize.define('Usuario', {
+  // Model attributes are defined here
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  nascimento:{
+    type: DataTypes.DATE
+  }
+}, {
+  // Other model options go here
+});
+
+
+async function sincronizar(){
+  await Usuario.sync();
+  console.log("Sincronizado");
+} 
+
+// sincronizar();
+
+async function criarUsuarios(usuario){
+  await Usuario.create(usuario);
+  console.log("Usuario criado");
+}
+
+// criarUsuarios({
+//   nome: "Jumenta do IF",
+//   email: "jumentadoif@gmail.com",
+//   nascimento: "2000-01-01"
+// });
+
+async function listarUsuarios(){
+  const usuarios = await Usuario.findAll();
+  console.log(JSON.stringify(usuarios, null, 2));
+}
+
+// listarUsuarios();
+
+async function buscarUsuarios(email){
+  const usuarios = await Usuario.findByPk(email);
+  console.log(JSON.stringify(usuarios));
+}
+
+// buscarUsuarios("jumentadoif@gmail.com");
+
+async function atualizarUsario(email, nome){
+  const usuario = await Usuario.findByPk(email);
+  usuario.nome = nome;
+  await usuario.save();
+  console.log("Usuarios atualizados");
+}
+
+// atualizarUsario("tulio@gmail.com", "Flavio Tulio Almeida");
+
+async function deletarUsario(email){
+  const usuario = await Usuario.findByPk(email);
+  await usuario.destroy();
+  console.log("Usuarios deletado");
+}
+
+deletarUsario("Joao@gmail.com");
